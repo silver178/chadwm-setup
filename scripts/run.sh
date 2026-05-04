717188c9a2a8b5021ae3a757214b281d67a5d535
Silver <71855839+silver178@users.noreply.github.com.>
2026-05-03 19:57:44 -0700

    Added fix for Nvidia GPUs and Picom not syncing the refresh rate correctly

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 xrdb merge ~/.Xresources
+# xrandr --output OUTPUTNAME --mode RESOLUTION --rate REFRESHRATE
 xrandr --output HDMI-0 --mode 1920x1080 --rate 60
+# Turns off sleep mode. Can be verified with xset q
+xset s off -dpms &
+# Basic exports
 export BROWSER=firefox
 export IMAGE_VIEWER=eog
+export EDITOR=nvim
 xbacklight -set 10 &
 #feh --bg-fill ~/Pictures/wall/gruv.png &
+# I hate policykeys so damn much
+lxqt-policykit-agent &
 ~/.fehbg &
 xset r rate 200 50 &
-picom &
+picom --backend glx --vsync &
 
 dash ~/.config/chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
